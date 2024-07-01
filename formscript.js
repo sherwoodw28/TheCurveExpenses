74ee379fea36c4dff9369fc698a4712f46cd2380
mariam123321 <m.mahmoud2307@gmail.com>
2024-07-01 14:26:28 +0100

    Add files via upload

---
+++ formscript.js
@@ -0,0 +1,7 @@
+document.getElementById('button1').addEventListener('click', function() {
+    window.location.href = 'reimbursement.html';
+});
+
+document.getElementById('button2').addEventListener('click', function() {
+    window.location.href = 'approval.html';
+});
