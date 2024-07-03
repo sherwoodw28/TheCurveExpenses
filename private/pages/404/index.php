3266a33ae1cd6dcb5c358209fc12f234505a740a
Eric Rosa <ericrosa914@gmail.com>
2024-07-03 10:13:46 +0100

    Added 404 Page

--- private/pages/404/index.php
+++ private/pages/404/index.php
@@ -5,9 +5,9 @@
 </head>    
 <body>
     <div class="content">
-        <h1>404: Page Not Found</h1>
-        <p>We're sorry, but the page you're looking for cannot be found.</p>
         <img src="/assets/404/image.png" alt="404 Image" class="error-image">
+        <h1>Page Not Found</h1>
+        <p>We're sorry, but the page you're looking for cannot be found.</p>
     </div>
 </body>
 </html>
